dc6a5b22bfa1275f35a8ecf1946d291a1c1601a5
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-06-20 22:03:23 +0300

    Add project files.

---
+++ Homework1/DataUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    public static class DataUtility
+    {
+        public static string? GetStrData(string initMessage, string? nullValueMessage = null)
+        {
+            string? result;
+            bool isNullValue = false;
+
+            Console.WriteLine(initMessage);
+            do
+            {
+                if (isNullValue)
+                    Console.WriteLine(nullValueMessage);
+
+                result = GetClearStr(Console.ReadLine());
+
+                isNullValue = result == null && nullValueMessage != null;                    
+
+
+            } while (isNullValue);
+
+            return result;
+        }
+
+
+        private static string? GetClearStr(string? value)
+        {
+            if (value == null)
+                return null;
+
+            value = value.Trim();
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            else
+                return value;
+        }
+    }
+}
---
+++ Homework1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintUserData("Данные некорректны... Попробуйте ещё раз!");
+        }
+
+        private static void PrintUserData(string nullValueMessage)
+        { 
+
+            string firstName = DataUtility.GetStrData("Введите Ваше имя", nullValueMessage);
+            Console.WriteLine();
+            string lastName = DataUtility.GetStrData("Введите Вашу фамилию", nullValueMessage);
+
+            Console.WriteLine();
+            Console.WriteLine($"Здравствуйте, {firstName} {lastName}!");
+        }
+    }
+}
