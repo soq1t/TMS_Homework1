b89e5e82bb3d914452b740949fd572782ed4a7e3
Andrei Gruzinov <andrei_gruzinov@list.ru>
2024-06-27 14:28:12 +0300

    Добавил ввод данных пользователем

---
+++ Homework3/DataUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public static class DataUtility
+    {
+        public static double GetDouble(string paramName)
+        {
+            bool isInputCorrect = true;
+            double result;
+
+            do
+            {
+                if (!isInputCorrect)
+                {
+                    Console.WriteLine("Неверный формат числа!");
+                    Console.WriteLine();
+                }
+
+                Console.Write($"Введите число {paramName}: ");
+                string input = Console.ReadLine();
+
+                isInputCorrect = double.TryParse(input, out result);
+            } while (!isInputCorrect);
+
+            return result;
+        }
+    }
+}
--- Homework3/Program.cs
+++ Homework3/Program.cs
@@ -2,9 +2,23 @@
 {
     internal class Program
     {
+        private static double x;
+        private static double y;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetInputNumbers();
+        }
+
+        private static void GetInputNumbers()
+        {
+            x = DataUtility.GetDouble("X");
+            Console.WriteLine();
+            y = DataUtility.GetDouble("Y");
+
+            Console.WriteLine();
+            Console.WriteLine("Ваши числа:");
+            Console.WriteLine($"X = {x}\nY = {y}");
         }
     }
 }
